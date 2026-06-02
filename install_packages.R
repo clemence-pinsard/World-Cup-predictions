2deee547767377e8fa9a0696037450f51279cf9f
clemence-pinsard <clemence.pinsard@eleve.ensai.fr>
2026-06-02 15:57:13 +0300

    update of install_packages

--- install_packages.R
+++ install_packages.R
@@ -3,4 +3,26 @@
 library(dplyr)
 library(devtools)
 
+# Package with the data (all the matches)
 install_github("martj42/international_results")
+
+# Package we need for footBayes
+install.packages("cmdstanr", repos = c("https://mc-stan.org/r-packages/", getOption("repos")))
+cmdstanr::install_cmdstan(overwrite = TRUE)
+
+# FootBayes package installation
+install_github("LeoEgidi/footBayes")
+
+# Check
+cmdstanr::check_cmdstan_toolchain()
+library(footBayes)
+packageVersion("footBayes")           
+instantiate::stan_cmdstan_exists()
+
+# Other packages
+install.packages(c("posterior", "bayesplot", "loo", "ggrepel"))
+
+library(posterior)
+library(bayesplot)
+library(loo)
+library(ggrepel)
