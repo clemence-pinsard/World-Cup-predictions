ab5d4035f36dceb76026fe13fa56c865c74dfa57
id2733 <clemence.pinsard@eleve.ensai.fr>
2026-06-04 09:46:28 +0000

    first try

--- script.R
+++ script.R
@@ -4,12 +4,33 @@ fit <- stan_foot(data = results_footBayes_groupstage1,
                  ranking = wc2026_ranking,
                  dynamic_type = "seasonal",
                  home_effect = FALSE,
-                 save_cmdstan_config = TRUE)
+                 save_cmdstan_config = TRUE,
+                 init = 0)
 
 prob <- foot_prob(fit, results_footBayes_groupstage1)
 colnames(prob$prob_table) <- c("home", "away",
                                "home win", "draw", "away win", "mlo")
 knitr::kable(prob$prob_table)
 #save prob$prob_table as csv
-write.csv(prob$prob_table, paste0(TABLES_PATH,"groupstage_1.csv"), row.names = FALSE)
+write.csv(prob$prob_table, "groupstage_1.csv", row.names = FALSE)
 prob$prob_plot
+
+
+
+
+fit_double_pois <- stan_foot(data = results_footBayes_groupstage1,
+                 model="double_pois",
+                 predict= ngames_groupstage1,
+                 ranking = wc2026_ranking,
+                 dynamic_type = "seasonal",
+                 home_effect = FALSE,
+                 save_cmdstan_config = TRUE,
+                 init = 0)
+
+prob_dp <- foot_prob(fit_double_pois, results_footBayes_groupstage1)
+colnames(prob_dp$prob_table) <- c("home", "away",
+                               "home win", "draw", "away win", "mlo")
+knitr::kable(prob_dp$prob_table)
+#save prob$prob_table as csv
+write.csv(prob_dp$prob_table, "groupstage_1.csv", row.names = FALSE)
+prob_dp$prob_plot
